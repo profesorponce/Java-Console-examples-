f1f2ec38fa56781c395008cf6447609541b382aa
profesorponce <142937098+profesorponce@users.noreply.github.com>
2023-08-24 12:37:06 -0300

    Add files via upload
    Este programa sirve para controlar la salida de consola del IDE de programación que usted use en el curso. Usa librerías estándar y no requiere librerías adicionales

---
+++ 01.Test.Consola.java
@@ -0,0 +1,20 @@
+// Test Consola en Java
+//
+// (c) Ricardo Ponce
+// https://www.profesorponce.blogspot.com
+// Junio 2022
+// 
+// Este programa sirve para controlar la salida de consola
+// del IDE de programación que usted use en el curso
+// 
+// Usa librerías estándar y no requiere librerías adicionales
+
+public class TestConsola
+{
+    public static void main(String[] args){
+
+	System.out.println ("Mi programa Java");
+
+    } //fin del main
+
+} // fin de la clase
